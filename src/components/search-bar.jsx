e79e12d2eb265be821f78866f0ac49c170408d81
Sidronio Lima <sidronio@sidroniolima.com.br>
2017-07-18 17:01:15 -0300

    Commit initial

---
+++ src/components/search-bar.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+export default class SearchBar extends Component
+{
+    constructor(props)
+    {
+        super(props)
+
+        this.state = { term: '' }
+    }
+
+    onInputChange(event)
+    {
+        this.setState({ term: event.target.value })
+    }
+
+    onFormSubmit(event)
+    {
+        event.preventDefault()
+    }
+
+    render ()
+    {
+        return (
+            <form onSubmit={ (event) => this.onFormSubmit(event) } 
+                className="input-group">
+                <input 
+                    placeholder="Get a five-day forecast in your favorite cities"
+                    className="form-control"
+                    value={ this.state.term }
+                    onChange={ (event) => this.onInputChange(event) } />
+
+                <span className="input-group-btn">
+                    <button type="submit" className="btn btn-secondary">Submit</button>
+                </span>
+            </form>
+        )
+    }
+}
